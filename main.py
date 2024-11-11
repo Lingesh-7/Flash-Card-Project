2d9a15bc2e15fe8bb0f6d7f46b03f6797c872182
Lingesh-7 <lingesh.r.official@gmail.com>
2024-11-11 11:51:03 +0530

    Add project

---
+++ main.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import random as r
+import pandas as p
+import time
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card={}
+w={}
+
+# ---------------------------- Flip card ------------------------------- #
+def flip():
+    canva.itemconfigure(ct,text="English",fill="white")
+    canva.itemconfig(cw,text=current_card['English'],fill="white")
+    canva.itemconfig(cb,image=backimg)
+
+
+# ---------------------------- Words ------------------------------- #
+try:
+    data=p.read_csv("words_to_learn.csv")
+except FileNotFoundError:
+    orgdata=p.read_csv(r"C:\Users\linge\Desktop\python 100 days\projects\pr-31\data\french_words.csv")
+    w=orgdata.to_dict(orient="records")
+else:
+    w=data.to_dict(orient="records")
+
+def words():
+    global current_card,fliptim
+    win.after_cancel(fliptim)
+    current_card=r.choice(w)
+    canva.itemconfig(ct,text="French",fill="black")
+    canva.itemconfig(cw,text=f"{current_card['French']}",fill="black")
+    canva.itemconfig(cb,image=fimg)
+    fliptim=win.after(3000,func=flip)
+
+
+
+
+# ---------------------------- Known Words ------------------------------- #
+def knownwords():
+    w.remove(current_card)
+    da=p.DataFrame(w)
+    da.to_csv("words_to_learn.csv",index=False)
+    words()
+        
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+win = Tk()
+win.title("Flash Cards")
+win.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+# win.after(3000,func=flip)
+# win.minsize(900,900)
+
+fliptim=win.after(3000,func=flip)
+
+fimg=PhotoImage(file=r"C:\Users\linge\Desktop\python 100 days\projects\pr-31\images\card_front.png")
+rimg=PhotoImage(file=r"C:\Users\linge\Desktop\python 100 days\projects\pr-31\images\right.png")
+wimg=PhotoImage(file=r"C:\Users\linge\Desktop\python 100 days\projects\pr-31\images\wrong.png")
+backimg=PhotoImage(file=r"C:\Users\linge\Desktop\python 100 days\projects\pr-31\images\card_back.png")
+
+canva=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
+cb=canva.create_image(400,263,image=fimg)
+ct=canva.create_text(400,150, font=('ariel',40,"italic"))
+cw=canva.create_text(400,263,font=('ariel',60,'bold'))
+canva.config(bg=BACKGROUND_COLOR)
+canva.grid(row=0,column=0,columnspan=2)
+
+wb=Button(image=wimg,highlightthickness=0,bg=BACKGROUND_COLOR,command=words)
+wb.grid(row=1,column=0)
+
+rgb=Button(image=rimg,highlightthickness=0,bg=BACKGROUND_COLOR,command=knownwords)
+rgb.grid(row=1,column=1)
+
+
+words()
+
+win.mainloop()
